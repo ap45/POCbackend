adb122fe37b4db2c80508409e97fd825002ff969
ap45 <arpit.parekh0104@gmail.com>
2024-10-15 23:44:31 -0400

    initial commit

---
+++ src/main/java/com/example/uncwpoc/Course.java
@@ -0,0 +1,4 @@
+package com.example.uncwpoc;
+
+public class Course {
+}
---
+++ src/main/java/com/example/uncwpoc/UncwpocApplication.java
@@ -0,0 +1,13 @@
+package com.example.uncwpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncwpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncwpocApplication.class, args);
+	}
+
+}
